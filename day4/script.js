84c1f5de7f7f00ca26054653be5086947f0e1084
anup7488 <147988934+anup7488@users.noreply.github.com>
2024-04-05 23:32:14 +0530

    Add files via upload

---
+++ day4/script.js
@@ -0,0 +1,13 @@
+// let a =new string ("hello ");
+// let b="hello ";
+// if(a==b){
+//     console.log("yes");
+
+
+// }
+// else{
+//     console.log("no");
+// }
+const obj={
+    name: "anup",
+}
